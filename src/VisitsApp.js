8fbf688ec73a1d7b4ed31f801737f45f96c47197
Pablo Selin Carrasco Armijo <pabloseline@gmail.com>
2023-01-04 16:11:17 +0100

    styling

--- src/VisitsApp.js
+++ src/VisitsApp.js
@@ -7,6 +7,7 @@ import VisitCanvas from "./VisitCanvas";
 
 function VisitsApp() {
   const [visits, setVisits] = useState(null);
+  const [infoVisible, setInfoVisible] = useState(false);
   const [palimpsesto, setPalimpsesto] = useState(false);
 
   useEffect(() => {
@@ -30,21 +31,26 @@ function VisitsApp() {
   }, [visits]);
 
   return (
-    <main className="main">
-      <h1>Representación de visitas a una web</h1>
+    <main className="main VisitsApp">
+      <header className="fixed_kit_header">
+      <h1>Representación de visitas a una web <span className="infoToggle" onClick={() => setInfoVisible(!infoVisible)}>[?]</span></h1>
+      {visits && <p>{visits.length} visitas registradas a <span className="urlInfo">{window.location.href}</span></p>} <TrackVisit />
+      <div className={`info ${infoVisible ? 'expanded' : 'hidden'}`}>
+      <h2>Notas</h2>
       <p>
         Cada símbolo representa una visita a esta web.
+        </p>
         <ul>
           <li>El tono del color cambia según el mes.</li>
           <li>La luminosidad corresponde a la hora de la visita.</li>
           <li>La forma del símbolo se genera a partir de la IP y el tipo de dispositivo.</li>
       </ul> 
-      </p>
+      </div>
+      </header>
       
 
       {visits && (
         <>
-          {visits.length} visitas registradas. <TrackVisit />
           <VisitCanvas palimpsesto={palimpsesto} visits={visits} />
         </>
       )}
